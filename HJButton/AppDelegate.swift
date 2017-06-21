797836ff58d5e7c8629e052832a32b22d9475b5b
name <yake.wang@liwushuo.com>
2017-06-21 21:41:07 +0800

    third commit

--- HJButton/AppDelegate.swift
+++ HJButton/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var str: String?
 
+    var nickname: String?
+    var hello: String?
     var view: UIView?
     var name: String?
 
