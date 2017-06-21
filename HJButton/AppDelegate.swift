d26270d42384f8d578dd224eb27e165b6bf9c9d2
gitph101 <keynote@quniandeMacBook-Pro.local>
2017-06-21 21:28:19 +0800

    sss

--- HJButton/AppDelegate.swift
+++ HJButton/AppDelegate.swift
@@ -14,6 +14,7 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var str: String?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
